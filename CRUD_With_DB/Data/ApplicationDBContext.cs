24fe8929966d116ec262e217be3643c3f201917e
dinesh_252649 <dinesh_252649@yahoo.com>
2025-01-12 01:38:21 +0530

    Add DB class

--- CRUD_With_DB/Data/ApplicationDBContext.cs
+++ CRUD_With_DB/Data/ApplicationDBContext.cs
@@ -1,6 +1,14 @@
-﻿namespace CRUD_With_DB.Data
+﻿using CRUD_With_DB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_With_DB.Data
 {
-    public class ApplicationDBContext
+    public class ApplicationDBContext:DbContext
     {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options) { 
+        
+        }
+        public DbSet<People> Peoples { get; set; }
+        
     }
 }
